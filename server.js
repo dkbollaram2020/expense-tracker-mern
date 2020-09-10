d5d65b0d3b2c714a2f4a5e8296a05030a33cc7b4
Dileep <dkbollaram2020@gmail.com>
2020-09-10 18:08:13 +0100

    adding CORS Headers

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ const transactions = require('./routes/transactions');
 
 const app = express();
 
+// CORS Headers => Required for cross-origin/ cross-server communication
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'development') {
